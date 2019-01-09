6817bd94303ea583e78380cafabf7fbbd940c7d3
muturan <muturan@L-T580-002.be.devoteam.com>
2019-01-09 10:04:25 +0100

    commit by Murat Turan

---
+++ Object Repository/test2.rs
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>test2</name>
+   <tag></tag>
+   <elementGuidId>8244da74-3d32-417b-a033-0855d207c97d</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>http://vod-service.be.obo.lgi.io/v2/detailscreen/crid:~~2F~~2Ftelenet.be~~2F73e6cd72-a9ed-40c0-8440-cbf696d87445?country=be&amp;language=nl&amp;profileId=71f38aef-b688-4b34-a52d-b4ce3cccbcd7_be~~23MasterProfile&amp;type=vod</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
